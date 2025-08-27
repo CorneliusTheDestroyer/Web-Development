e4cfe8b92defbc97aa0c474cfcd84a7ad6f36535
CorneliusTheDestroyer <cornestols7@gmail.com>
2025-08-27 13:21:51 +0200

    New Commit

--- 17.0 Dicee+Challenge+-+Starting+Files/index.js
+++ 17.0 Dicee+Challenge+-+Starting+Files/index.js
@@ -1,1 +1,21 @@
 var randomNumber1 = Math.floor(Math.random() * 6) + 1;
+var randomNumber2 = Math.floor(Math.random() * 6) + 1;
+
+var img1 = document.getElementsByClassName("img1");
+var img2 = document.getElementsByClassName("img2");
+var heading = document.querySelector("h1");  
+
+img1[0].setAttribute("src", "images/dice" + randomNumber1 + ".png");
+img2[0].setAttribute("src", "images/dice" + randomNumber2 + ".png");
+
+heading.textContent = "New Heading!";
+
+if (randomNumber1 > randomNumber2) {
+    heading.textContent = "Player One Wins!";
+}    
+if (randomNumber1 < randomNumber2) {
+    heading.textContent = "Player Two Wins!";
+} 
+if (randomNumber1 === randomNumber2) {
+    heading.textContent = "Draw!";
+} 
