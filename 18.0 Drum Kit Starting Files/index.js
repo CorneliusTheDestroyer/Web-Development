c1089dd0384eb98de909525b9325a0163756bb22
Cornelius <cornestols7@gmail.com>
2025-09-03 16:49:25 +0200

    New Commit

--- 18.0 Drum Kit Starting Files/index.js
+++ 18.0 Drum Kit Starting Files/index.js
@@ -4,7 +4,8 @@ var numberOfDrumButtons = document.querySelectorAll(".drum").length;
 for (var i = 0; i < numberOfDrumButtons; i++) {
 
 document.querySelectorAll(".drum")[i].addEventListener("click", function (){
-    alert("I got clicked!");
+    var sound = new Audio('./sounds/tom-1.mp3');
+    sound.play();
 });    
 
 }
