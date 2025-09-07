9eff5c6430bfc6f6e6cfc3a1b6920bf561c11d83
CorneliusTheDestroyer <cornestols7@gmail.com>
2025-09-07 10:41:46 +0200

    New Commit

---
+++ 23 - BackEnd/2.3 NPM/index.js
@@ -0,0 +1,12 @@
+// var generateName = require("sillyname");
+ import generateName from "sillyName";
+
+ var sillyName = generateName();
+
+ console.log(`My name is ${sillyName}.`);
+
+
+// import superheroes from "superheroes";
+// const name = superheroes.random();
+
+// console.log(`My super hero name is ${name}.`);
