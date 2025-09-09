9bc04ec3013b6be29ebc2711ad688abf5aa4d20e
CorneliusTheDestroyer <cornestols7@gmail.com>
2025-09-09 14:23:11 +0200

    New Commit

--- 23 - BackEnd/3.2 HTTP Request/index.js
+++ 23 - BackEnd/3.2 HTTP Request/index.js
@@ -3,9 +3,17 @@ const app = express();
 const port = 3000;
 
 app.get("/", (req, res) => {
-    console.log("Welcome");
+    res.send("<h1>Welcome!</h1>");
 })
 
-app.request(port, () => {
-    console.log(`Listening to port ${port}`)
+app.get("/about", (req, res) => {
+    res.send("<h1>About</h1>")
+})
+
+app.get("/contact", (req, res) => {
+    res.send("<h1>Contact</h1>");
+})
+
+app.listen(port, () => {
+    console.log(`Server running on port ${port}.`)
 })
